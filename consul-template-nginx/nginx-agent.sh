7ff28634b1d95bfe8dae8f7558119cd97f83aa4b
wyb <1326058785@qq.com>
2017-04-28 17:27:46 +0800

    nginx-agent.sh

---
+++ consul-template-nginx/nginx-agent.sh
@@ -0,0 +1,31 @@
+
+
+#!/bin/sh
+#
+# 命令:
+#   sudo sh nginx-agent.sh [CONSUL_IP] 
+# 参数解释： 
+#  CONSUl_IP  consul 地址
+#
+#变量
+CONSUL_IP=${1}
+container_name=nginx_agent
+##
+echo "--- Download coss-nginx:1.0 Image ---"
+#docker pull coss-nginx:1.0
+
+##
+docker inspect --format '{{.Name}}' ${container_name}
+if [ $? -eq 0 ]; then 
+	docker stop ${container_name} && docker rm -fv ${container_name}
+fi
+echo "--- Nginx Auto Load Balancer Install ---"
+docker run -d \
+    --restart=always \
+    --name ${container_name} \
+    -p 80:80 \
+    -e 'CONSUL_URL=${CONSUL_IP}' \
+    coss-nginx:1.0
+if [ $? -eq 0 ];then
+	echo "--- Nginx Auto LoadBalancer Install Success!!! ---"	
+fi
