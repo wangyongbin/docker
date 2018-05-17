89f026e866d1711085910e92cebfc7c42d65903c
wyb <1326058785@qq.com>
2018-05-18 00:16:15 +0800

    添加端口

--- images/mongo.sh
+++ images/mongo.sh
@@ -1,5 +1,8 @@
 docker volume create vol_mongo
+docker volume create vol_mongo_configdb
 
 docker run --restart=always -d --name dev-mongo \
 -v vol_mongo:/data/db \
-mongo
+-v vol_mongo_configdb:/data/configdb \
+-p 27017:27017 \
+mongo:3.7
